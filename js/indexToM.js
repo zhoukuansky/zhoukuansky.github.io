c87bfe04f8e65593556c5ddf14022cbeb5075fc8
zhoukuansky <zhoukuansky@163.com>
2020-04-20 21:43:48 +0800

    add new

--- js/indexToM.js
+++ js/indexToM.js
@@ -1,9 +1,9 @@
 // //如果屏幕宽度过小，转往移动端
 // if (window.innerWidth <= 900) {
-//     location.href = "mini.html";
+//     location.href = "./mini.html";
 // }
 // window.onresize = function () {
 //     if ($(window).width() <= 900) {
-//         location.href = "mini.html";
+//         location.href = "./mini.html";
 //     }
 // }
--- js/mToindex.js
+++ js/mToindex.js
@@ -1,9 +1,9 @@
 //如果屏幕宽度过宽，转往PC端
 if (window.innerWidth > 900) {
-    location.href = "index.html";
+    location.href = "./index.html";
 }
 window.onresize = function () {
     if ($(window).width() > 900) {
-        location.href = "index.html";
+        location.href = "./index.html";
     }
 }
